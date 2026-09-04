714199ed6a1d0082f344927d9e7073f46cdaf86f
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-09-04 09:19:55 +0530

    Add files via upload

---
+++ BackEnd/src/server.js
@@ -0,0 +1,13 @@
+const dns = require('node:dns');
+dns.setServers(['1.1.1.1', '8.8.8.8']);
+
+require("dotenv").config();
+const app = require("./app.js");
+const connectDB = require("./config/db.js")
+
+connectDB().then(() => {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+  });
+});
